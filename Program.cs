d75d62b3cdb1b2ef6956f4a6e810bc14fa577b3c
Genesis Hernandez <genesisccp3@gmail.com>
2026-05-04 20:11:25 -0600

    Primer push con estructura base de proyecto y configuracion de git ignore

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// ============================================================
+// SISTEMA DE INVENTARIO - Clase 1.1
+// Estado: Mensaje de bienvenida
+// ============================================================
+
+using System.Reflection;
+
+var assembly =Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+Console.WriteLine("==========================================");
+Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
+Console.WriteLine("==========================================");
+Console.WriteLine();
+Console.WriteLine($"Versión:{version}");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+Console.WriteLine();
+Console.WriteLine("Estructura del proyecto:");
+Console.WriteLine("Configuracion .csproj");
+Console.WriteLine("Carpeta src/ creada");
+Console.WriteLine("Metadatos confugurados");
+Console.WriteLine();
+Console.WriteLine("Proximo paso: Agregar argumentos CLI y configuracion de repositorio en GitHub");
